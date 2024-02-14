6063c1aa70324a20231cd01fbb5c4083523f7cb9
Jonathan Boada <jonathanboada1@gmail.com>
2024-02-14 13:24:43 -0500

    Proyecto finalizado, sourcemaps, cssnano,postcss,autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,28 @@ const { src, dest, watch, parallel } = require("gulp");
 // Importación de plugins
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 //Imagenes
 const cache = require('gulp-cache')
 const imagemin = require('gulp-imagemin')
 const avif = require('gulp-avif');
 
+//Java script
+
+const terser = require('gulp-terser-js');
+
 // Tarea para compilar CSS desde Sass
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo SASS
+    .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass()) // Compilarlo
+    .pipe(postcss([autoprefixer(), cssnano]))
+    .pipe(sourcemaps.write("."))
     .pipe(dest("build/css")); // Almacenarla en el disco duro
 
   done(); // Callback que avisa a gulp cuando llegamos al final
@@ -67,6 +78,9 @@ function versionAvif(done) {
 
 function javascript(done) {
   src('src/js/**/*.js')
+  .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
     .pipe(dest('build/js'));
   done();
 }
